7764f3e5c83c7c7f93a4501b967a0bc92c271cba
root <saadelhaziti2002@gmail.com>
2023-03-23 08:08:51 -0700

    betty

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+}
