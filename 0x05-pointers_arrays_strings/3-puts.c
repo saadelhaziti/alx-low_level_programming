aa01be1c04425c71c0f62e711c550a2f57281717
saad <saadelhaziti2002@example.com>
2023-03-27 14:23:46 +0000

    task 0

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,17 +2,17 @@
 
 /**
  * _puts -> Prints a string
- * 
- * @str : Input
+ *
+ * @str: Input
  */
 
 void _puts(char *str)
 {
-    int i;
+	int i;
 
-    for ( i = 0 ; str[i] != '\0' ; i++)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
