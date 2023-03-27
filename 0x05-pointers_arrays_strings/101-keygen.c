13beaceb5533ce5711fbf7b233726ae5c3bd06aa
saad <saadelhaziti2002@example.com>
2023-03-27 15:28:40 +0000

    uptodate

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random passwords 101-crackme
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+    int rang;
+    srand(time(NULL));
+    i=0;
+    while (i<=20)
+    {
+        rang= rand()%128;
+        putchar(rang);
+        i++;
+    }
+
+    putchar((rang+423)*128);
+	return (0);
+}
