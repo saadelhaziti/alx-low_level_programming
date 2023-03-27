87bd6e3d4727b58616dfb160493ce5c63e2db33f
saad <saadelhaziti2002@example.com>
2023-03-27 14:32:59 +0000

    task 0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev -> prints a string, in reverse
+ * 
+ * @s: Input
+ */
+
+void print_rev(char *s)
+{
+    int i;
+
+    for (i = strlen(s)-1; i >= 0; i--)
+    {
+        _putchar(s[i]);
+    }
+    _putchar('\n');
+}
