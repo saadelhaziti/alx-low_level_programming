c09ed054857c6f0260b6ae8e816b6aeef4848d67
saad <saadelhaziti2002@example.com>
2023-03-30 13:12:41 +0000

    task

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * *leet -> Leet encode
+ *
+ * @str: Input String
+ *
+ * Return: String
+ */
+
+char *leet(char *str)
+{
+int i;
+
+for (i = 0 ; str[i] != '\0' ; i++)
+{
+if (str[i] == 'a' || str[i] == 'A')
+{
+str[i] = '4';
+}
+if (str[i] == 'e' || str[i] == 'E')
+{
+str[i] = '3';
+}
+if (str[i] == 'o' || str[i] == 'O')
+{
+str[i] = '0';
+}
+if (str[i] == 't' || str[i] == 'T')
+{
+str[i] = '7';
+}
+if (str[i] == 'l' || str[i] == 'L')
+{
+str[i] = '1';
+}
+}
+return (str);
+}
