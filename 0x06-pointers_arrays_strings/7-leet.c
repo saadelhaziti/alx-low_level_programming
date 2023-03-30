373840c99f9e485cf3c1987bab0915f9b617f8f4
saad <saadelhaziti2002@example.com>
2023-03-30 13:18:00 +0000

    task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,16 +12,16 @@ char *leet(char *str)
 {
 	int	i;
 	int	j;
-	char *al = "aAeEoOtTlL";
-	char *nb = "4433007711";
+	char *a = "aAeEoOtTlL";
+	char *n = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (str[i] == al[j])
+			if (str[i] == a[j])
 			{
-				str[i] = nb[j];
+				str[i] = n[j];
 			}
 		}
 	}
