77a19d4999b51c4f2ffc823a3d9d4f8eaf0149ac
root <saadelhaziti2002@example.com>
2023-04-05 16:06:41 -0700

    task

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _strlen ->  Length of a string
+ *
+ * @s: Input
+ *
+ * Return: The length
+ */
+
+int _strlen(char *s)
+{
+if (*s == '\0')
+return (0);
+s++;
+return (_strlen(s) + 1);
+}
+
+/**
+ * _compare -> Compare
+ *
+ * @s: Inpute String
+ * @len: String Length
+ *
+ * Return: Depend Condition
+ */
+
+int _compare(char *s, int fin)
+{
+if (fin < 1)
+return (1);
+if (*s == *(s + fin))
+{
+s++;
+fin -= 2;
+return (_compare(s, fin));
+}
+return (0);
+}
+
+/**
+ * is_palindrome -> Palindrome string
+ *
+ * @s: Inpute String
+ *
+ * Return: Depend Condition
+ */
+
+int is_palindrome(char *s)
+{
+int fin;
+
+fin = _strlen(s) - 1;
+return (_compare(s, fin));
+}
