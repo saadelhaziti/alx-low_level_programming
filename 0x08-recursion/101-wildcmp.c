9109b7db3015aae01fff0e3f4bfaa38ede0816c2
root <saadelhaziti2002@example.com>
2023-04-05 16:57:45 -0700

    task

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,35 +1,6 @@
 #include "main.h"
 
 /**
- * wildcmp_helper ->  wildcmp helper
- *
- * @s1: First Input
- * @s2: Scond Input
- *
- * Return: The length
- */
-
-int wildcmp_helper(char *s1, char *s2)
-{
-    if (*s1 == '\0' && *s2 == '\0') {
-        return 1;
-    }
-    if (*s2 == '*') {
-        if (wildcmp_helper(s1, s2+1) == 1) {
-            return 1;
-        }
-        if (*s1 != '\0' && wildcmp_helper(s1+1, s2) == 1) {
-            return 1;
-        }
-        return 0;
-    }
-    if (*s1 == *s2) {
-        return wildcmp_helper(s1+1, s2+1);
-    }
-    return 0;
-}
-
-/**
  * wildcmp -> Compare string
  *
  * @s1: First Input
@@ -40,17 +11,11 @@ int wildcmp_helper(char *s1, char *s2)
 
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '\0') {
-        return (*s1 == '\0');
-    }
-    if (*s2 == '*') {
-        if (wildcmp_helper(s1, s2+1) == 1) {
-            return 1;
-        }
-        return (wildcmp(s1+1, s2) == 1);
-    }
-    if (*s1 == *s2) {
-        return wildcmp(s1+1, s2+1);
-    }
-    return 0;
+if (*s2 == '\0')
+return (*s1 == '\0');
+if (*s2 == *s1)
+return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+if (*s2 == '*')
+return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+return (0);
 }
