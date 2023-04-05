1c4ba859725422a296cd289ce70a4c895de6fc25
root <saadelhaziti2002@example.com>
2023-04-05 14:50:20 -0700

    task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -> Prints a string, followed by a new line.
+ *
+ * @s: Input
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+_putchar('\n');
+if (*s != '\0')
+{
+_putchar(*s);
+s++;
+_puts_recursion(s);
+}
+}
