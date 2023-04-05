45994bfdaa5e0da43a3281131d222f678676874e
root <saadelhaziti2002@example.com>
2023-04-05 15:22:59 -0700

    task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,20 +3,23 @@
 /**
  * _sqrt -> Square root of a number
  *
- * @n: Input Number
+ * @x: Input Number
  * @i: Parameter
  *
  * Return: Depend Condition
  */
 
-int _sqrt(int n, int i)
+int _sqrt(int x, int i)
 {
-if (n < 0 || i * i > n)
+int nb = 0;
+
+if (x < 0 || i * i > x)
 return (-1);
-if (i * i == n)
+if (i * i == x)
 return (i);
 i++;
-return (_sqrt(n, i));
+nb = _sqrt(x, i)
+return (nb);
 }
 
 /**
