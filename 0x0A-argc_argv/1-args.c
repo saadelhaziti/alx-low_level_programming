b059c48e47e5b432aacaeca3b8b37bb6c9a5c09c
saad <saadelhaziti2002@example.com>
2023-04-08 21:38:07 +0000

    task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -> Program
+ *
+ * @argc: Size array argv
+ * @argv: Array of size argc
+ *
+ * Return: 0 Always
+ */
+int main(int argc, char **argv)
+{
+int num;
+
+num = argc - 1;
+printf("%d\n", num);
+return (0);
+}
