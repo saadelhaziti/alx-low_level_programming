31d4496256fc4eaa881020ffc09c28606ad32324
saad <saadelhaziti2002@example.com>
2023-04-08 21:32:43 +0000

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -> Program
+ *
+ * @argc: Size array argv
+ * @argv: Array of size argc
+ *
+ * Return: 0 Always
+ */
+
+int main(int argc, char **argv)
+{
+argc = 0;
+printf("%s\n", argv[argc]);
+return (0);
+}
