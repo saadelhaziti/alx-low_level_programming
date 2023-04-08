b215111a0e4e27ed4b8e6113ee1b74e5f21845dc
saad <saadelhaziti2002@example.com>
2023-04-08 22:00:52 +0000

    task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,25 +11,25 @@
 int _atoi(char *s)
 {
 int i;
-int sin;
-unsigned int digit;
+int si;
+unsigned int d;
 
 i = 0;
-sin = 1;
-digit = 0;
+si = 1;
+d = 0;
 while (s[i] != '\0')
 {
 if (s[i] == '-')
-sin *= -1;
+si *= -1;
 else if (s[i] >= '0' && s[i] <= '9')
 {
-digit = (digit * 10) + (s[i] - '0');
+d = (d * 10) + (s[i] - '0');
 }
-else if (digit > 0)
+else if (d > 0)
 break;
 i++;
 }
-return (digit * sin);
+return (d *si);
 }
 
 /**
@@ -45,27 +45,27 @@ int main(int argc, char **argv)
 {
 int i;
 int l;
-int num;
-int coins[] = {25, 10, 5, 2, 1};
+int n;
+int c[] = {25, 10, 5, 2, 1};
 
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-num = _atoi(argv[1]);
-if (num < 0)
+n = _atoi(argv[1]);
+if (n < 0)
 {
 printf("0\n");
 return (0);
 }
 l = 0;
-for (i = 0; i < 5 && num >= 0; i++)
+for (i = 0; i < 5 && n >= 0; i++)
 {
-while (num >= coins[i])
+while (n >= c[i])
 {
 l++;
-num -= coins[i];
+n -= c[i];
 }
 }
 
