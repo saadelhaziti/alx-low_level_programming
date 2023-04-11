f7848ed0f4c43546a23e21ae1322d077f2f54b5d
saadel2002 <93224461+saadel2002@users.noreply.github.com>
2023-04-11 22:10:49 +0000

    task and readme

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array ->Create array
+ *
+ * @size: Input Array
+ * @c: Input String
+ *
+ * Return: Depend Condition
+ */
+char *create_array(unsigned int size, char c)
+{
+char *tab;
+int i;
+
+if (size == 0)
+return (NULL);
+tab = (char *) malloc(size * sizeof(char));
+if (tab == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+{
+tab[i] = c;
+}
+return (tab);
+}
