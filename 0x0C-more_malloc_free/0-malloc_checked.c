337eeaf1730a37f257b0be8cd237a7897970796e
saad <saadelhaziti2002@example.com>
2023-04-13 22:05:40 +0000

    task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked -> Allocates memory using malloc
+ *
+ * @b: Input
+ *
+ * Return: Void Pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *m;
+
+	m = (void *) malloc(b);
+	if (m == NULL)
+		exit(98);
+	return (m);
+}
