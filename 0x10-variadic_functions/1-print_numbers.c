9d9aeb6ea802489d39ab2b277251dba867402005
saad <saadelhaziti2002@example.com>
2023-04-20 18:35:02 +0000

    all task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,6 +3,31 @@
 #include "variadic_functions.h"
 
 /**
+ * print_number -> prints an integer
+ *
+ * @n: input
+ */
+
+void print_number(int n)
+{
+unsigned int m;
+
+if (n < 0)
+{
+m = -n;
+putchar('-');
+}
+else
+m = n;
+if (m / 10)
+{
+print_number(m / 10);
+}
+
+putchar((m % 10) + '0');
+}
+
+/**
  * print_numbers -> Prints numbers, followed by a new line.
  *
  * @separator: Input String Between Numbers
@@ -12,14 +37,23 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
-va_list list;
+unsigned int j;
+int num;
+va_list argv;
 
-va_start(list, n);
+va_start(argv, n);
 for (i = 0; i < n; i++)
 {
-printf("%d", va_arg(list, const int));
-printf("%s ", separator);
+num = va_arg(argv, const unsigned int);
+print_number(num);
+if (separator != NULL && i < n - 1)
+{
+for (j = 0; separator[j] != '\0'; j++)
+{
+putchar(separator[j]);
+}
+}
 }
 putchar('\n');
-va_end(list);
+va_end(argv);
 }
