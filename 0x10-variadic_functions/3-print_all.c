fbba2df215d2fa492fefc0fda77e6ac9b2914568
saad <saadelhaziti2002@example.com>
2023-04-20 21:26:14 +0000

    all task

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * _string-> print string
+ * @var: Args list
+*/
+
+void p_string(va_list var)
+{
+char *str;
+
+str = va_arg(var, char *);
+if (str != NULL)
+{
+printf("%s", str);
+return;
+}
+printf("(nil)");
+}
+
+/**
+ * _float-> print float
+ * @var: Args list
+*/
+
+void _float(va_list var)
+{
+float x;
+
+x = va_arg(var, double);
+printf("%f", x);
+}
+
+/**
+ * _int-> print int
+ * @var: Args list
+*/
+
+void _int(va_list var)
+{
+int x;
+
+x = va_arg(var, int);
+printf("%d", x);
+}
+
+/**
+ * _char-> print char
+ * @var: Args list
+*/
+
+void _char(va_list var)
+{
+char x;
+
+x = va_arg(var, int);
+printf("%c", x);
+}
+
+/**
+ * print_all -> Prints anything.
+ * @format: Args list
+ */
+
+void print_all(const char * const format, ...)
+{
+int i; 
+int j;
+char *spechlle = "";
+va_list args;
+
+string func[] = {
+{"c", _char}, {"i", _int}, {"f", _float}, {"s", p_string}
+};
+
+i = 0;
+va_start(args, format);
+while (format && (*(format + i)))
+{
+j = 0;
+while (*(format +i) != *(func[j].str))
+{
+j++;
+}
+if (j != 0)
+{
+printf("%s", spechlle);
+func[j].point(args);
+spechlle = ", ";
+}
+i++;
+}
+printf("\n");
+va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,8 +4,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+typedef struct forma_cara
+{
+	char *str;
+	void (*point)(va_list arg);
+} string;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
