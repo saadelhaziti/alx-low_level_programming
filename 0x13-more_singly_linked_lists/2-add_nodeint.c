01a31a03a798d7e66e15e50ab4675be8f060b561
saadelhaziti <saadelhaziti2002@gmail.com>
2023-04-29 12:21:13 +0100

    task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len -> func of print liste
+ * @head: double pointre for start
+ * @n: intger
+ * Return: listint_t
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new_noude;
+
+new_noude = (listint_t*)malloc(sizeof(listint_t));
+if (new_noude == NULL)
+{
+return (NULL);
+}
+new_noude->n = n;
+new_noude->next = *head;
+*head = new_noude;
+return (*head);
+}
